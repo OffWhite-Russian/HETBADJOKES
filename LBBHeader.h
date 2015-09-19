27d05f3cf090919dd9fdb53df29cb44ff29b9301
Dan Golant <dgolant@gmail.com>
2015-09-19 12:40:35 -0400

    oh god rick i hope this works

---
+++ LBBHeader.h
@@ -0,0 +1,14 @@
+//
+//  LBBHeader.h
+//  
+//
+//  Created by Dan on 9/19/15.
+//
+//
+
+#ifndef _LBBHeader_h
+#define _LBBHeader_h
+
+#import <PTDBeanManager.h>
+
+#endif
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '7.0'
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'HET' do
+    pod 'Bean-iOS-OSX-SDK', '~> 0.9'
+end
